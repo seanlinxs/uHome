a48a7f0883767fd0b4ee71d08f15b06ada93cf95
Sean Lin <sean.lin@live.com>
2014-10-07 01:06:47 +1100

    Initial commit

---
+++ uHome/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(uHome.Startup))]
+namespace uHome
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
