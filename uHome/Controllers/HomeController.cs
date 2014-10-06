bcddd2bc514d9f4e1200b85169247eb5b157f544
Sean Lin <sean.lin@live.com>
2014-10-07 02:39:50 +1100

    Add set culture action

--- uHome/Controllers/HomeController.cs
+++ uHome/Controllers/HomeController.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using uHome.Helpers;
 
 namespace uHome.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
@@ -26,5 +25,27 @@ namespace uHome.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult SetCulture(string culture)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+            // Save culture in a cookie
+            HttpCookie cookie = Request.Cookies["_culture"];
+
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+
+            Response.Cookies.Add(cookie);
+            
+            return RedirectToAction("Index");
+        } 
     }
 }
