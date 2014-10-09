6259663070c58406a68bd01af2508fe5b1926576
Sean Lin <sean.lin@live.com>
2014-10-10 02:15:16 +1100

    Change namespace if various managers

--- uHome/App_Start/IdentityConfig.cs
+++ uHome/App_Start/IdentityConfig.cs
@@ -10,9 +10,8 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using uHome.Models;
 
-namespace uHome
+namespace uHome.Models
 {
     public class EmailService : IIdentityMessageService
     {
