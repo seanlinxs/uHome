264f824ad670fe9b9b5489f9f9ebf9f1789dce94
Lin Xiao Song <sean.linxs@gmail.com>
2014-10-27 03:23:36 +1100

    Refine video index view

--- uHome/Global.asax.cs
+++ uHome/Global.asax.cs
@@ -19,5 +19,32 @@ namespace uHome
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+
+            if (ex is HttpRequestValidationException)
+            {
+                Response.Clear();
+                Response.StatusCode = 200;
+                Response.Write(@"
+<html>
+  <head>
+    <title>HTML Not Allowed</title>
+  </head>
+  <body>
+    <h1>Oops!</h1>
+    <p>I'm sorry, but HTML entry is not allowed on that page.</p>
+    <p>
+      Please make sure that your entries do not contain any angle brackets like &lt; or &gt;.
+    </p>
+    <p><a href='javascript:back()'>Go back</a></p>
+  </body>
+</html>
+");
+                Response.End();
+            }
+        }
     }
 }
