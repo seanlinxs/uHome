ca40109850bf983e549c44da2b6de0b4620dee47
Lin Xiao Song <sean.linxs@gmail.com>
2014-11-01 05:12:59 +1100

    First authorization unit test

---
+++ uHome/Authorization/UhomeResourceAuthorizationManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Thinktecture.IdentityModel.Owin.ResourceAuthorization;
+
+namespace uHome.Authorization
+{
+    public class UhomeResourceAuthorizationManager : ResourceAuthorizationManager
+    {
+        public override Task<bool> CheckAccessAsync(ResourceAuthorizationContext ctx)
+        {
+            var resource = ctx.Resource.First().Value;
+
+            if (resource == UhomeResources.VideoClip)
+            {
+                return CheckAccessVideoClipAsync(ctx);
+            }
+
+            return Nok();
+        }
+
+        public Task<bool> CheckAccessVideoClipAsync(ResourceAuthorizationContext ctx)
+        {
+            var user = ctx.Principal.Identity;
+
+            if (! user.IsAuthenticated)
+            {
+                return Nok();
+            }
+
+            return Nok();
+        }
+    }
+}
---
+++ uHome/Authorization/UhomeResources.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace uHome.Authorization
+{
+    public class UhomeResources
+    {
+        public const string VideoClip = "VideoClip";
+        public class VideoClipActions
+        {
+            public const string View = "View";
+        }
+
+    }
+}
