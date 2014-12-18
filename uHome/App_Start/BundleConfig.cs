0992742c6adaa9350119d2fe03ecba47fbd5a9c9
Sean Lin <sean.lin@live.com>
2014-12-19 10:49:15 +1100

    Remove uplodify

--- uHome/App_Start/BundleConfig.cs
+++ uHome/App_Start/BundleConfig.cs
@@ -10,7 +10,6 @@ namespace uHome
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*", "~/Scripts/jquery.unobtrusive*"));
-            bundles.Add(new ScriptBundle("~/bundles/uploadify").Include("~/Scripts/jquery.uploadify.js"));
             bundles.Add(new ScriptBundle("~/bundles/plupload").Include("~/Scripts/plupload/plupload.full.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -18,7 +17,6 @@ namespace uHome
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css", "~/Content/uploadify.css"));
             bundles.Add(new ScriptBundle("~/bundles/jssor").Include("~/Scripts/jssor.*"));
 
             // Set EnableOptimizations to false for debugging. For more information,
