a29c68865b24241190e59ea744041c5eef5e32ea
Sean Lin <sean.lin@live.com>
2015-01-02 15:03:00 +1100

    Create event view model class

---
+++ uHome/Models/EventViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using uHome.Annotations;
+
+namespace uHome.Models
+{
+    public class CreateEventViewModel
+    {
+        [LocalizedRequired]
+        [LocalizedStringLength(50)]
+        public string Title { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        public DateTime OpenAt { get; set; }
+        public string Address { get; set; }
+        public HttpPostedFileBase Poster { get; set; }
+    }
+}
